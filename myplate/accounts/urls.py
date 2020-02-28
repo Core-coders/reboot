8baa46bf34c8804634d41ac2df40ba0f3b03d2f8
unknown <rithinja@gmail.com>
2020-02-28 23:48:37 +0530

    sorted and models migrated

--- myplate/accounts/urls.py
+++ myplate/accounts/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login',views.login_view,name="login"),
-    path('test',views.test_view,name="test"),
-    path('test2',views.test2_view,name="test2"),
-    path('test3',views.test3_view,name="test3"),
-    path('logout',views.logout_view,name="logout"),
+    path('login/',views.login_view, name="login"),
+    path('test2',views.test2_view, name="test2"),
+    path('logout',views.logout_view, name="logout"),
 ]
--- myplate/accounts/views.py
+++ myplate/accounts/views.py
@@ -3,14 +3,11 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout
 from django.http import HttpResponse
 
-def test_view(request):
-    return render(request,'accounts/test')
+
 
 def test2_view(request):
     return render(request,'accounts/test2.html')
-
-def test3_view(request):
-    return render(request,'accounts/test3.html')   
+  
 
 def login_view(request):
     if request.method == 'POST':
@@ -19,19 +16,18 @@ def login_view(request):
             user = form.get_user()
             if str(form.get_user()) == 'HO':
                 login(request,user)
-                return render(request,'accounts/test.html')
+                return render(request,'accounts/test2.html')
             elif str(form.get_user()) == 'HM':
                 login(request,user)
                 return render(request,'accounts/test2.html')
             elif str(form.get_user()) == 'NF':
                 login(request,user)
-                #return HttpResponse(user)
-                return render(request,'accounts/test3.html')
+                return render(request,'accounts/test2.html')
             else:
                 return HttpResponse('better luck next time')
             
     else:
-         form = AuthenticationForm()
+        form = AuthenticationForm()
     return render(request,'accounts/login.html',{'form':form})
     
 def logout_view(request):
