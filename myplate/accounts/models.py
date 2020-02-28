f2288625c6385215fe193cd09556c3a5833f15df
unknown <rithinja@gmail.com>
2020-02-28 21:16:48 +0530

    login functionality sorted and models migrated

--- myplate/accounts/models.py
+++ myplate/accounts/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Hmdetails(models.Model):
+    hmname = models.CharField(max_length=50)
+    school = models.CharField(max_length=50)
+    address = models.TextField(max_length=100)
+
+class Studentdetails(models.Model):
+    name = models.CharField(max_length=100)
+    bloodgroup = models.CharField(max_length=100)
+    dob = models.DateTimeField()
+    aadhar = models.IntegerField()
+
+
 
